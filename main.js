16d266f907a82f0eafb6203b459b3b209af53d5b
Towsif Ahmed Omi <towsif031@gmail.com>
2018-10-15 00:03:56 +0600

    Added desktop notification alert feature

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const {app, BrowserWindow, Menu} = require ('electron')
-const path = require ('path')
-const url = require ('url')
+const {app, BrowserWindow, Menu} = require('electron')
+const path = require('path')
+const url = require('url')
 const shell = require('electron').shell
 const ipc = require('electron').ipcMain
 
@@ -9,31 +9,31 @@ const ipc = require('electron').ipcMain
 let win
 
 function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow ({width: 800 ,height: 600})
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join (__dirname, 'src/index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'src/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-    // Open the DevTools.
-    win.webContents.openDevTools()
+  // Open the DevTools.
+  win.webContents.openDevTools()
 
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Difference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null
-    })
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
 
-    var menu = Menu.buildFromTemplate([
-        {
-            label: 'Menu',
-            submenu: [
+  var menu = Menu.buildFromTemplate([
+      {
+          label: 'Menu',
+          submenu: [
                 {label: 'Adjust Notification Value'},
                 {
                     label: 'CoinMarketCap',
@@ -47,12 +47,15 @@ function createWindow () {
                     click() {
                         app.quit()
                     }
-                },
-            ]
-        }
-    ])
+                }
+          ]
+      },
+      {
+          label: 'Info'
+      }
+  ])
 
-    Menu.setApplicationMenu(menu);
+  Menu.setApplicationMenu(menu)
 }
 
 // This method will be called when Electron has finished
@@ -60,23 +63,23 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all winodws are closed.
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock item is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
-    }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
 })
 
 ipc.on('update-notify-value', function(event, arg) {
-    win.webContents.send('targetPriceVal', arg)
+  win.webContents.send('targetPriceVal', arg)
 })
--- src/index.js
+++ src/index.js
@@ -7,26 +7,38 @@ const ipc = electron.ipcRenderer
 const notifyBtn = document.getElementById('notifyBtn')
 var price = document.querySelector('h1')
 var targetPrice = document.getElementById('targetPrice')
+var targetPriceVal
+
+const notification = {
+    title: 'BTC Alert',
+    body: 'BTC just beat your target price!',
+    icon: path.join(__dirname, '../assets/images/btc.png')
+}
 
 function getBTC() {
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
         .then(res => {
             const cryptos = res.data.BTC.USD
             price.innerHTML = '$'+cryptos.toLocaleString('en')
+
+            if (targetPrice.innerHTML != '' && targetPriceVal < res.data.BTC.USD) {
+                const myNotification = new window.Notification(notification.title, notification)
+            }
+
         })
 }
 getBTC()
-setInterval(getBTC, 30000)
+setInterval(getBTC, 10000)
 
 notifyBtn.addEventListener('click', function(event) {
     const modalPath = path.join('file://', __dirname, 'add.html')
     let win = new BrowserWindow({ frame: false, transparent: true, alwaysOnTop: true, width: 400, height: 200})
-    win.on('close', function () { win = null })
+    win.on('close', function() { win = null })
     win.loadURL(modalPath)
     win.show()
 })
 
-ipc.on('targetPriceVal', function(event, arg) {
+ipc.on('targetPriceVal', function (event, arg) {
     targetPriceVal = Number(arg)
     targetPrice.innerHTML = '$'+targetPriceVal.toLocaleString('en')
 })
